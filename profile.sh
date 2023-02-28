8444c908870f1291d1097166e4411e8e7e715dc5
Amit Kumar <amit2.kumar@intel.com>
2023-02-28 23:47:47 +0530

    Update profile.sh

--- profile.sh
+++ profile.sh
@@ -8,36 +8,62 @@ set -a
 #this is provided while using Utility OS
 source /opt/bootstrap/functions
 
-
-
 # --- Add Packages
-ubuntu_packages="wget binutils ubuntu-desktop openssh-server qemu-system-x86"
+ubuntu_packages=""
 
-# --- List out any docker images you want pre-installed separated by spaces. ---
-pull_sysdockerimagelist=""
+img_url=""
+if [[ $kernel_params == *" img_url="* ]]; then
+	tmp="${kernel_params##* img_url=}"
+	export img_url="${tmp%% *}"
+else
+	update_flashing_status "ISO image download" "Failed" "10%" "image url is empty"
+	run "sending /tmp/provisiong.log to esp" "python3 send_log_file.py" "/tmp/provisioning.log"
+	exit 1
+fi
 
-# --- List out any docker tar images you want pre-installed separated by spaces.  We be pulled by wget. ---
-wget_sysdockerimagelist="" 
+uuid=""
+if [[ $kernel_params == *" uuid="* ]]; then
+	tmp="${kernel_params##* uuid=}"
+	export uuid="${tmp%% storage_type*}"
+fi
 
+storage_type=""
+if [[ $kernel_params == *" storage_type="* ]]; then
+	tmp="${kernel_params##* storage_type=}"
+	export storage_type="${tmp%% *}"
+fi
 
+drive_arguments=''
+if [ ! -z "$uuid" ]; then
+	echo "UUID is: ${uuid}"	2>&1 | tee -a /dev/console
+    drive_arguments="--uuid \"${uuid}\""
+elif [ ! -z "$storage_type" ]; then
+	echo "Storage type is: ${storage_type}" 2>&1 | tee -a /dev/console
+	drive_arguments="--storage_type ${storage_type}"
+else
+	echo "Both UUID & Storage_type are empty" 2>&1 | tee -a /dev/console
+	drive_arguments=""
+fi
 
-# --- Install Extra Packages ---
-run "Installing Extra Packages on Ubuntu ${param_ubuntuversion}" \
+run "Arguments for iso_image_flashing.py" "echo -e '\n img_url:${img_url} \n uuid:${uuid} \n storage_type:${storage_type}'" "/tmp/provisioning.log"
+
+# --- Install ubuntu kernel and Debian Packages ---
+run "Installing kernel and Debian Packages on Ubuntu ${param_ubuntuversion}" \
     "docker run -i --rm --privileged --name ubuntu-installer ${DOCKER_PROXY_ENV} -v /dev:/dev -v /sys/:/sys/ -v $ROOTFS:/target/root ubuntu:${param_ubuntuversion} sh -c \
     'mount --bind dev /target/root/dev && \
     mount -t proc proc /target/root/proc && \
     mount -t sysfs sysfs /target/root/sys && \
-    LANG=C.UTF-8 chroot /target/root sh -c \
-        \"$(echo ${INLINE_PROXY} | sed "s#'#\\\\\"#g") export TERM=xterm-color && \
-        export DEBIAN_FRONTEND=noninteractive && \
-        ${MOUNT_DURING_INSTALL} && \
-        apt install -y ${ubuntu_packages}\"'" \
+    chmod 666 /dev/null && \
+    apt update && \
+    apt-get -y install python3-pip && \
+    pip3 install  pip-system-certs && \
+    pip3 install --force-reinstall star_fw -i https://ubit-artifactory-ba.intel.com/artifactory/api/pypi/star_pip_packages-ba-local/simple/ --extra-index-url https://pypi.org/simple/ --no-cache-dir && \
+    apt-get -y install sudo && \
+    apt-get -y install fdisk && \
+    apt-get -y install efibootmgr && \
+    apt-get -y install wget && \
+    apt-get -y install curl && \
+    apt install -y lshw && \
+    wget --header \"Authorization: token ${param_token}\" ${param_basebranch}/iso_image_flashing.py && \
+    python3 iso_image_flashing.py  --img_url ${img_url} --esp_host ${param_httpserver} --sut_mac ${macaddr} ${drive_arguments}'" \
     ${PROVISION_LOG}
-
-# --- Pull any and load any system images ---
-for image in $pull_sysdockerimagelist; do
-	run "Installing system-docker image $image" "docker exec -i system-docker docker pull $image" "$TMP/provisioning.log"
-done
-for image in $wget_sysdockerimagelist; do
-	run "Installing system-docker image $image" "wget -O- $image 2>> $TMP/provisioning.log | docker exec -i system-docker docker load" "$TMP/provisioning.log"
-done
